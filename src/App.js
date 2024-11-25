7fe6f8aee4a77166428784c61631b3f9ec9cc952
yunuseyvz <yunus.yavuz@campus.lmu.de>
2024-11-26 00:32:08 +0100

    revert that

--- src/App.js
+++ src/App.js
@@ -6,12 +6,10 @@ import {
   Redirect,
 } from 'react-router-dom';
 import { get, isNil } from 'lodash';
-import myLogo from './logo.svg';
 
 import Lobby from './containers/Lobby';
 import Game from './containers/Game';
 import './App.css';
-import ReactPWAInstallProvider, { useReactPWAInstall } from "react-pwa-install";
 
 function App() {
   const [auth, setAuth] = useState({
@@ -20,34 +18,9 @@ function App() {
     roomID: null,
   });
 
-  const { pwaInstall, supported, isInstalled } = useReactPWAInstall();
-
-  const handleClick = () => {
-    pwaInstall({
-      title: "Install Web App",
-      logo: myLogo,
-      features: (
-        <ul>
-          <li>Cool feature 1</li>
-          <li>Cool feature 2</li>
-          <li>Even cooler feature</li>
-          <li>Works offline</li>
-        </ul>
-      ),
-      description: "This is a very good app that does a lot of useful stuff. ",
-    })
-      .then(() => alert("App installed successfully or instructions for install shown"))
-      .catch(() => alert("User opted out from installing"));
-  };
-
   return (
     <div className="App">
       <Router>
-        {supported() && !isInstalled() && (
-          <button type="button" onClick={handleClick}>
-            Install App
-          </button>
-        )}
         <Switch>
           <Route
             path="/:id"
--- src/index.js
+++ src/index.js
@@ -3,13 +3,10 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
-import ReactPWAInstallProvider, { useReactPWAInstall } from "react-pwa-install";
 
 ReactDOM.render(
   <React.StrictMode>
-    <ReactPWAInstallProvider enableLogging>
-      <App />
-    </ReactPWAInstallProvider>,
+    <App />
   </React.StrictMode>,
   document.getElementById('root')
 );
